d37d3f837353c56a0b9f5fe28beac405a666ea1a
Lindsey Heagy <lindseyheagy@gmail.com>
2020-04-24 19:17:29 -0600

    seperate the generation of the training data from the ML pipeline

--- uxo_utils/imports.py
+++ uxo_utils/imports.py
@@ -4,6 +4,7 @@ import os
 code_dir = os.path.sep.join(["..", "..", "UXO_protected", "+BTInvertPY"])
 
 sys.path.append(code_dir)
+
 from BTSensor import (
     SensorInfo, Model, preCalcLoopCorners, FModParam,
     forwardWithQ, sensorCoords2RxCoords, hprimary, formQmatrix,
